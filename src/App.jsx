635f39ef8d990a5c6e4ce24f807ce6bafbf70fe9
xhentu <heinchanthu@yahoo.com>
2025-07-26 21:43:45 +0630

    changing ClipProfile.jsx name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import BackgroundCanvas from './components/BackgroundCanvas.jsx';
-import ClipProfile from './components/clipProfile.jsx';
+import ClipProfile from './components/ClipProfile.jsx';
 import TerminalContainer from './components/TerminalContainer.jsx';
 import EmailMe from "./components/EmailMe";
 
